c8ec414b8a79e39d6f7b34e3f787e912ccb47fc6
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 12:56:17 -0700

    build

--- src/server/index.js
+++ src/server/index.js
@@ -4,14 +4,15 @@ const server = require('http').Server(app);
 const io = (module.exports.io = require('socket.io')(server));
 
 const PORT = process.env.PORT || 3231;
+
 const SocketManager = require('./SocketManager');
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '../../build');
+	res.sendFile(__dirname + '/../../build');
 });
+
 io.on('connection', SocketManager);
 
 server.listen(PORT, () => {
-	console.log('Connected to port', PORT);
+	console.log('Connected to port:' + PORT);
 });
-//build 2
