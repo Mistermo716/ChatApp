5fbbda36de2d95baad99b11873c10089b08f4c02
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 04:42:48 -0700

    127.0.0.1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import LoginForm from './LoginForm';
 import Typing from 'react-typing-animation';
 import ChatContainer from './ChatContainer';
 
-const socketUrl = process.env.SOCKET_URL || 'http://192.168.1.7:3231';
+const socketUrl = process.env.SOCKET_URL || 'http://127.0.0.1:3231';
 export default class Layout extends Component {
 	constructor(props) {
 		super(props);
