33abcab1a8915b1f69151d16d576cd1f879f11f5
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 02:25:02 -0700

    work

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const io = (module.exports.io = require('socket.io')(server));
 
 const PORT = process.env.PORT || 3231;
 const SocketManager = require('./SocketManager');
-app.use(express.static(__dirname + './../../build'));
+app.use(express.static('../../build'));
 io.on('connection', SocketManager);
 
 server.listen(PORT, () => {
