bc9ba53d50b301b3cacc160361d347e52a52f258
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 02:14:49 -0700

    final test

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,13 @@
-var http = require('http');
-var express = require('express'),
-	app = (module.exports.app = express());
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = (module.exports.io = require('socket.io')(server));
 
-var server = http.createServer(app);
-var io = require('socket.io').listen(server); //pass a http.Server instance
 const PORT = process.env.PORT || 3231;
-server.listen(PORT); //listen on port 80
 const SocketManager = require('./SocketManager');
-
+app.use(express.static(__dirname + '/../../build'));
 io.on('connection', SocketManager);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log('Connected to port', PORT);
 });
