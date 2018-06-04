297967f0685096c283241700d02d8ce0624613c4
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 04:31:27 -0700

    trust the process

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,8 @@ import LoginForm from './LoginForm';
 import Typing from 'react-typing-animation';
 import ChatContainer from './ChatContainer';
 
-const socketUrl = process.env.SOCKET_URL || 'http://192.168.1.7:3231';
+const trustTheProcess = process.env.SOCKET_URL;
+const socketUrl = `https://${trustTheProcess}` || 'http://192.168.1.7:3231';
 export default class Layout extends Component {
 	constructor(props) {
 		super(props);
