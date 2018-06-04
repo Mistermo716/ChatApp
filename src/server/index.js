06f1a47f6451d7c23914f940d883fb28c84bed0f
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 02:30:51 -0700

    server

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const io = (module.exports.io = require('socket.io')(server));
 const PORT = process.env.PORT || 3231;
 const SocketManager = require('./SocketManager');
 
-app.get('/', function(req, res) {
+server.get('/', function(req, res) {
 	res.sendfile(__dirname + '../../index.html');
 });
 io.on('connection', SocketManager);
