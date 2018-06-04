0e1f72d7ad112184f9a0635031a7955a96a8dfa2
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 04:15:12 -0700

    localhost

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import LoginForm from './LoginForm';
 import Typing from 'react-typing-animation';
 import ChatContainer from './ChatContainer';
 
-const socketUrl = 'http://secure-forest-80226.herokuapp.com/:3231';
+const socketUrl = 'http://localhost:3231';
 export default class Layout extends Component {
 	constructor(props) {
 		super(props);
