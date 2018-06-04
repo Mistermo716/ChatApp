0713e5d386e5b245839e323a14f2b7ff67d97265
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 03:25:17 -0700

    npm start

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const server = require('http').Server(app);
 const io = (module.exports.io = require('socket.io')(server));
 
 const PORT = process.env.PORT || 3231;
 const SocketManager = require('./SocketManager');
 
-// handle every other route with index.html, which will contain
-// a script tag to your application's JavaScript file(s).
-app.get('*', function(request, response) {
-	response.sendFile(path.resolve(__dirname, '../../build'));
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '../../build');
 });
 io.on('connection', SocketManager);
 
