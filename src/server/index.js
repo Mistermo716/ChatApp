d0a929456c10fd52c60dccedc3fa49476d9370e7
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 03:12:54 -0700

    build

--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,12 @@ const io = (module.exports.io = require('socket.io')(server));
 const PORT = process.env.PORT || 3231;
 const SocketManager = require('./SocketManager');
 // define the folder that will be used for static assets
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function(request, response) {
-	response.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
+	response.sendFile(path.resolve(__dirname, '../../build/public', 'index.html'));
 });
 io.on('connection', SocketManager);
 
