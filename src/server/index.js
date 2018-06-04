e2354c39a0a7257f96b4bec5d89b5a5365133934
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 02:42:54 -0700

    non error build

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const io = (module.exports.io = require('socket.io')(server));
 
 const PORT = process.env.PORT || 3231;
 const SocketManager = require('./SocketManager');
-app.use(express.static(__dirname + '/../../public'));
+app.use(express.static(__dirname + '/../../build'));
 io.on('connection', SocketManager);
 
 server.listen(PORT, () => {
