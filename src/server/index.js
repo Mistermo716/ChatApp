d87fd969977b04ef1773f3414882c88f8477858c
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-04 13:05:15 -0700

    test

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const cors = require('cors');
 const io = (module.exports.io = require('socket.io')(server));
 
 const PORT = process.env.PORT || 3231;
 
 const SocketManager = require('./SocketManager');
 
+app.use(cors());
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/../../build');
 });
